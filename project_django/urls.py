fe9777c4da6c462e1e8463ec4b98dd9290458824
JuanPablo_Tututi <darksoul011@live.com>
2020-02-23 15:22:49 -0600

    Nuevo modulo

--- project_django/urls.py
+++ project_django/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import path, include
 from django.contrib import admin
 # from django.urls import path
 from api.resources import PostResource, UserResource
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 post_resource = PostResource()
 user_resource = UserResource()
